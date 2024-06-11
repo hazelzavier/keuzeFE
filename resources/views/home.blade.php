0eab5bdcba3078499b5c05fa0b6296283d58525d
Hazel Davian <davianhazel@gmail.com>
2024-06-11 23:32:50 +0200

    working about me page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,11 +17,76 @@
 
         <!-- About Me Section -->
         <section id="about" class="min-h-screen md:min-h-screen-75 flex items-center justify-center bg-gray-800 overflow-y-auto pb-20">
-            <div class="container mx-auto py-8">
+            <div class="container mx-auto py-8 text-center">
                 <h1 class="text-5xl font-bold text-cyan-500 mb-4">About Me</h1>
-                <div class="text-lg text-gray-300">
-                    <p>I have a passion for developing beautiful and functional websites...</p>
-                    <!-- Add more content as needed -->
+                <div class="text-lg text-gray-300 mx-auto max-w-2xl">
+                    <p class="mb-6">Welcome to my corner of the web! I'm Hazel Zavier Zehoshafhat Davian, a 20-year-old originally from Jakarta, Indonesia, but now proud to call the Netherlands home.</p>
+                    <div class="mb-8">
+                        <!-- Neon Text Effect -->
+                        <h3 class="text-3xl text-yellow-300 font-bold mb-2 tracking-wide">
+                            <span class="text-cyan-500">Gaming Enthusiast</span>, Tech Explorer, <span class="text-pink-400">Code Dreamer</span>
+                        </h3>
+                        <!-- Subtle Hover Effect -->
+                        <p class="mb-2 hover:underline">My journey into the world of technology began with a passion for gaming. From the adrenaline-fueled matches of CS to the open-world adventures of GTA, I've always been drawn to the immersive experiences that PC gaming offers. It all started with Counter Strike 1.6, where I first discovered the thrill of online competition and the endless possibilities of virtual worlds.</p>
+                        <p class="mb-2">As I delved deeper into gaming, I became increasingly curious about the inner workings of computers. I found myself tinkering with settings, exploring mods, and even dipping my toes into programming to enhance my gaming experience.</p>
+                        <!-- Subtle Hover Effect -->
+                        <p class="mb-2 hover:underline">With the encouragement of my mom, I discovered the world of software development. The idea of being able to create something tangible out of lines of code resonated deeply with me. There's a certain magic in writing a piece of code and seeing it come to life on the screen, functioning just as you intended.</p>
+                        <!-- Added Information -->
+                        <p class="mb-2">Since enrolling at Techniek College Rotterdam, I've expanded my skills in HTML5, CSS, JavaScript, PHP, Laravel, Tailwind CSS, and I've also gained some experience with Kotlin. Additionally, I completed a 6-month internship at ConfigWise as a Junior Software Developer.</p>
+                    </div>
+                    <div class="mt-6">
+                        <h2 class="text-2xl font-semibold text-cyan-400 mb-2">Skills</h2>
+                        <div class="flex flex-wrap justify-center">
+                            <div class="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
+                                <div class="border-4 border-cyan-500 rounded-lg p-4">
+                                    <h3 class="text-lg font-semibold text-cyan-300 mb-2">HTML/CSS</h3>
+                                    <div class="progress-bar h-4 bg-cyan-400 rounded-full mb-2">
+                                        <div class="progress-bar-inner h-full bg-cyan-600 rounded-full w-2/3"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
+                                <div class="border-4 border-cyan-500 rounded-lg p-4">
+                                    <h3 class="text-lg font-semibold text-cyan-300 mb-2">JavaScript</h3>
+                                    <div class="progress-bar h-4 bg-cyan-400 rounded-full mb-2">
+                                        <div class="progress-bar-inner h-full bg-cyan-600 rounded-full w-1/2"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
+                                <div class="border-4 border-cyan-500 rounded-lg p-4">
+                                    <h3 class="text-lg font-semibold text-cyan-300 mb-2">PHP</h3>
+                                    <div class="progress-bar h-4 bg-cyan-400 rounded-full mb-2">
+                                        <div class="progress-bar-inner h-full bg-cyan-600 rounded-full w-3/4"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
+                                <div class="border-4 border-cyan-500 rounded-lg p-4">
+                                    <h3 class="text-lg font-semibold text-cyan-300 mb-2">Laravel</h3>
+                                    <div class="progress-bar h-4 bg-cyan-400 rounded-full mb-2">
+                                        <div class="progress-bar-inner h-full bg-cyan-600 rounded-full w-3/5"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
+                                <div class="border-4 border-cyan-500 rounded-lg p-4">
+                                    <h3 class="text-lg font-semibold text-cyan-300 mb-2">Tailwind CSS</h3>
+                                    <div class="progress-bar h-4 bg-cyan-400 rounded-full mb-2">
+                                        <div class="progress-bar-inner h-full bg-cyan-600 rounded-full w-3/5"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
+                                <div class="border-4 border-cyan-500 rounded-lg p-4">
+                                    <h3 class="text-lg font-semibold text-cyan-300 mb-2">Kotlin</h3>
+                                    <div class="progress-bar h-4 bg-cyan-400 rounded-full mb-2">
+                                        <div class="progress-bar-inner h-full bg-cyan-600 rounded-full w-1/4"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
