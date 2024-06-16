cd8b3a44618e7f5e2b2c68c5e20d47d5a5c23e8d
Hazel Davian <davianhazel@gmail.com>
2024-06-16 23:44:48 +0200

    working everything + JS

--- resources/views/case-study.blade.php
+++ resources/views/case-study.blade.php
@@ -13,18 +13,18 @@
     </head>
     <body class="bg-gray-100 p-6">
     <div class="flex flex-col lg:flex-row items-center justify-center lg:justify-between p-4">
-        <div class="max-w-md lg:max-w-lg order-2 lg:order-1 mb-4 lg:mb-0">
+        <div class="max-w-md lg:max-w-lg order-2 lg:order-1 mb-4 lg:mb-0 lg:mr-4">
             <img src="https://placehold.co/300" alt="Case Study Image" class="rounded-lg shadow-lg">
         </div>
-        <div class="max-w-md lg:max-w-lg order-1 lg:order-2">
+        <div class="max-w-md lg:max-w-lg order-1 lg:order-2 lg:ml-4">
             <p class="text-lg text-center lg:text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
     </div>
     <div class="flex flex-col lg:flex-row-reverse items-center justify-center lg:justify-between p-4">
-        <div class="max-w-md lg:max-w-lg mb-4 lg:mb-0">
+        <div class="max-w-md lg:max-w-lg mb-4 lg:mb-0 lg:ml-4">
             <img src="https://placehold.co/300" alt="Case Study Image" class="rounded-lg shadow-lg">
         </div>
-        <div class="max-w-md lg:max-w-lg">
+        <div class="max-w-md lg:max-w-lg lg:mr-4">
             <p class="text-lg text-center lg:text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
     </div>
